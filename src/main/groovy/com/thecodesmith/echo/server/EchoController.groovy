66b6803f966ba7cd38ba23af613992d47d4e70cc
Brian Stewart <brian.stewart@jamf.com>
2019-08-28 09:49:55 -0500

    Support POST with JSON/XML

--- src/main/groovy/com/thecodesmith/echo/server/EchoController.groovy
+++ src/main/groovy/com/thecodesmith/echo/server/EchoController.groovy
@@ -1,15 +1,40 @@
 package com.thecodesmith.echo.server
 
+import groovy.json.JsonOutput
+import groovy.xml.XmlUtil
+
 import io.micronaut.http.HttpRequest
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Consumes
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Header
+import io.micronaut.http.annotation.Post
 
 @Controller("/")
 class EchoController {
 
     @Get('{path:.*}')
-    String echo(HttpRequest request, String path) {
-        println "Returning path $request.path"
-        "Path: $request.path"
+    String echo(String path) {
+        def response = "Request: GET /$path"
+        println response
+        response
+    }
+
+    @Post('{path:.*}')
+    @Consumes([MediaType.APPLICATION_JSON, MediaType.TEXT_XML])
+    String post(String path, @Body String body, @Header String contentType) {
+        def content = contentType == MediaType.APPLICATION_JSON ?
+                JsonOutput.prettyPrint(body) :
+                XmlUtil.serialize(body)
+
+        def response = """
+                |Request: POST /$path
+                |Content-Type: $contentType
+                |Body: \n$content """.stripMargin()
+
+        println response
+        response
     }
 }
